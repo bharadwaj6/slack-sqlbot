66a4ee95a9602513ec4482a566b23f0ae9ed85cc
bharadwaj6 <krishna.bharadwaj6@gmail.com>
2015-05-17 17:19:10 +0530

    changed settings layout

--- slack_sqlbot/settings.py
+++ slack_sqlbot/settings.py
@@ -53,16 +53,6 @@ ROOT_URLCONF = 'slack_sqlbot.urls'
 WSGI_APPLICATION = 'slack_sqlbot.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
