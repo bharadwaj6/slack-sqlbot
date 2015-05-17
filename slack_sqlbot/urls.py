b7395b9b803329f675ade07c9046f1a5c0aeff22
bharadwaj6 <krishna.bharadwaj6@gmail.com>
2015-05-17 19:36:48 +0530

    fixed syntax error in urls

--- slack_sqlbot/urls.py
+++ slack_sqlbot/urls.py
@@ -1,10 +1,5 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'slack_sqlbot.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^sql/$', 'sqlparser.views.parse_sql')
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^sql/$', 'sqlparser.views.parse_sql'),
+                       )
